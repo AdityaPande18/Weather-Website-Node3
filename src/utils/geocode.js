217c90d2d9b041873363895e7209686547d1e90d
Aditya Pande <adityapande88@gmail.com>
2020-06-05 10:27:56 +0530

    minor changes
    minor changes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,5 +1,6 @@
 const request = require('request')
 
+//Geocode request
 const geocode = (address, callback)=>{
     const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFydmVsNjkiLCJhIjoiY2s1YjA4YmM1MWNocTNtcGF1bzNoZTZwMSJ9.EXlWXm1KwQhYQ_fw3HYLEg'
     request({url, json:true}, (error, {body}) =>{
